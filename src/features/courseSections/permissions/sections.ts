140fddc45558f813973defa4009153bb45d9aa9e
Satyam Prajapati <satyamm1402@gmail.com>
2025-07-20 19:50:54 +0530

    [admin] Course Sections/Lessons Edit Page - create sections.ts actions in courseSections feature - create sectionSchema in courseSections feature - create permissions in courseSections feature - create insert, update, delete sections functions in courseSections/db feature - create SectionForm.tsx in features/courseSections/components/SectionForm.tsx - create SectionFormDialog.tsx in features/courseSections/components - create EditCoursePage component in admin/courses/[courseId]/edit but it's not showing the card-content

---
+++ src/features/courseSections/permissions/sections.ts
@@ -0,0 +1,15 @@
+// src/features/courseSections/permissions/sections.ts
+import { UserRole } from "@/drizzle/schema";
+
+
+export function canCreateCourseSections({ role }: { role: UserRole | undefined }) {
+  return role === "admin"
+}
+
+export function canUpdateCourseSections({ role }: { role: UserRole | undefined }) {
+  return role === "admin"
+}
+
+export function canDeleteCourseSections({ role }: { role: UserRole | undefined }) {
+  return role === "admin"
+}
---
+++ src/features/courseSections/schemas/sections.ts
@@ -0,0 +1,9 @@
+// src/features/courseSections/schemas/sections.ts
+
+import { courseSectionStatuses } from "@/drizzle/schema";
+import z from "zod";
+
+export const sectionSchema = z.object({
+  name: z.string().min(1, "Required"),
+  status: z.enum(courseSectionStatuses),
+})
